3c80fae648e0d76e05f3f2b50b9c945e7f98f1bb
Federico Russo <federico.russo.0513@gmail.com>
2024-04-14 20:53:05 +0200

    Completato lo scraping delle stats stagionali dei giocatori

--- DataScraper.py
+++ DataScraper.py
@@ -4,13 +4,29 @@ import numpy as np
 import basketball_reference_scraper.teams as teams
 import basketball_reference_scraper.players as players
 import basketball_reference_scraper.box_scores as boxScore
+import basketball_reference_scraper.injury_report as injury
 from SQLManager import insertPlayerStats, insertPlayerGameLog
 
-season = 2016
-nba_teams_2015_2016 = [
-    "ATL", "BOS", "BKN", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", 
+#seasons = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+seasons = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+
+nba_teams_2014_today = [
+    "ATL", "BOS", "BRK", "CHO", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", 
     "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOP", "NYK", 
-    "OKC", "ORL", "PHI", "PHX", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"
+    "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"
+]
+nba_teams_2013_2014 = ["ATL", "BOS", "BRK", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", 
+    "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOP", "NYK", 
+    "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
+
+nba_teams_2012_2013 = ["ATL", "BOS", "BRK", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", 
+    "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", 
+    "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
+
+nba_teams_from_2008 = [
+    "ATL", "BOS", "NJN", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", 
+    "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", 
+    "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"
 ]
 
 
@@ -27,17 +43,34 @@ def get_player_stats(player_name):
     insertPlayerStats(df)
 
 def get_roster_stats_season(team_name, season):
-    time.sleep(1)
-    df = teams.get_roster_stats(team_name, season)
-    df['Team'] = team_name
-    df['Season'] = season
-    insertPlayerStats(df)
+    while True:
+        try:
+            time.sleep(0.1)
+            df = teams.get_roster_stats(team_name, season)
+            df['Team'] = team_name
+            df['Season'] = season
+            insertPlayerStats(df)
+            break  # Break the loop if the request succeeds
+        except ConnectionError as e:
+            print("Error obtaining data table for ", team_name, " season: ", season, ":", e)
+            print("Retrying request in 5 seconds...")
+            time.sleep(5)
+            
+def load_roster_stats():
+    for season in seasons:
+        if(season < 2013):
+            for team in nba_teams_from_2008:
+                get_roster_stats_season(team, season)
+        if(season == 2013):
+            for team in nba_teams_2012_2013:
+                get_roster_stats_season(team, season)
+        if(season == 2014):
+            for team in nba_teams_2013_2014:
+                get_roster_stats_season(team, season)
+        if(season > 2014):
+            for team in nba_teams_2014_today:
+                get_roster_stats_season(team, season)
 
-#df = teams.get_roster_stats("GSW", 2022)
-#print(df.columns)
-
-for team in nba_teams_2015_2016:
-    get_roster_stats_season(team, season)
-#pd.concat([get_player_logs(player,season) for player in gsw['PLAYER']], ignore_index=True)
+load_roster_stats()
 
     
